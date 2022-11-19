e927f35ffed9278957f36381ca3dbd1af9f46dd9
Yul-Leonardo <46727018+Yul-Leonardo@users.noreply.github.com>
2022-11-19 19:39:44 +0100

    Update App.js

--- ylrg/src/App.js
+++ ylrg/src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1> YAGO ES TONTO </h1>
+      <h1> YAGO IST GAY </h1>
+      <img src="https://www.google.com/search?q=gay+flag&rlz=1C1CHBF_deDE926DE926&sxsrf=ALiCzsaVYRTuo1phyya7YcYTFto1iWMZVw:1668883022576&tbm=isch&source=iu&ictx=1&vet=1&fir=YGqikkju0VVaQM%252CsRYEquqkd14pRM%252C%252Fm%252F02x5z8q%253Bx4beLh4_kkSU0M%252CWx5JhOAInAZvlM%252C_%253BEgNiCRicLmRH8M%252Cgs5l0tvApu6bMM%252C_%253BtWi3qwmdwrPI7M%252CJR3nmHQACB6_mM%252C_%253B3SnSDZ3X1-VEFM%252Cddsv0pgje7-4lM%252C_%253BKthZudX6krY1LM%252CJrTgzgKepO9hCM%252C_&usg=AI4_-kRmlew9z1GxadDAkzDnYu9ydJximg&sa=X&ved=2ahUKEwiOvO3d8br7AhUjuqQKHaw3AYwQ_B16BAh7EAE&cshid=1668883081016637#imgrc=YGqikkju0VVaQM" />
     </div>
   );
 }
