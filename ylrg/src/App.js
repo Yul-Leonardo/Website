0a8d10c965bfbc17be0b9707cab6cb1b6f865e18
Yul-Leonardo <46727018+Yul-Leonardo@users.noreply.github.com>
2022-11-19 19:50:33 +0100

    Update App.js

--- ylrg/src/App.js
+++ ylrg/src/App.js
@@ -4,12 +4,13 @@ import Gay from './images/gay.png';
 function App() {
   return (
     <div classNome="App">
-      <div className="center">
+      {/* <div className="center">
         <h1> YAGO IST GAY </h1>
       </div>
       <div className="center">
         <img src={Gay} alt="Gay Flagge" />
-      </div>
+      </div> */}
+      <h1> Wann wollen wir uns treffen ? </h1>
     </div>
   );
 }
