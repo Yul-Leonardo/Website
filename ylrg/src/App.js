0cbeb1c67237ba40316321f452275588c5bfb030
Yul Leonardo Rusch Godoy <yulrusch@gmail.com>
2023-01-24 22:44:04 +0100

    todo list react

--- ylrg/src/App.js
+++ ylrg/src/App.js
@@ -1,19 +1,51 @@
 import './App.css';
+import React, { useState } from "react";
 import Gay from './images/gay.png';
 
 function App() {
+
+  const [list, setList] = useState([]);
+  const [input, setInput] = useState("");
+
+  const addTodo = (todo) => {
+    const newTodo = {
+      id: Math.random(),
+      todo: todo,
+    };
+
+    // add the todo to the list
+    setList([...list, newTodo]);
+
+    // clear input box
+    setInput("");
+  };
+
+  const deleteTodo = (id) => {
+    // Filter out todo with the id
+    const newList = list.filter((todo) => todo.id !== id);
+
+    setList(newList);
+  };
+
   return (
-    <div classNome="App">
-      <h1> YLRG </h1>
-      <h2> Make a TODO application ! </h2>
-      {/* <div className="center">
-        <h1> YAGO IST GAY </h1>
-      </div>
-      <div className="center">
-        <img src={Gay} alt="Gay Flagge" />
-      </div> */}
+    <div>
+      <h1>Todo List</h1>
+      <input 
+        type="text" 
+        value={input} 
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <button onClick={() => addTodo(input)}> Add </button>
+      <ul>
+        {list.map((todo) => (
+          <li key={todo.id}>
+            {todo.todo}
+            <button onClick={() => deleteTodo(todo.id)}>&times;</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
