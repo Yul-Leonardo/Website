0ff8b04fd3d90a532cc3af2f1af2a02f7397dfd9
Yul-Leonardo <yulrusch@gmail.com>
2024-04-21 17:41:14 +0200

    Updated!

--- ylrg/src/App.js
+++ ylrg/src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
+import FormGenerator from './components/FormGenerator'
 
 function App() {
   return (
-    <div>
-      <h1>
-        YLRG
-      </h1>
-      <h2>
-        Julius Cesar
-      </h2>
+    <div className="container">
+      <div className='item-3'>
+        <FormGenerator/>
+      </div>
+      <h1 className="item-1"> YLRG </h1>
+      <div className="item-2">
+
+      </div> 
     </div>
   );
 }
