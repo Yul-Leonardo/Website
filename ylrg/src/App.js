87d84f8b3d7572f96ce1fdcbc7f896bce615b418
Yul Leonardo Rusch Godoy <46727018+Yul-Leonardo@users.noreply.github.com>
2024-05-03 21:13:35 +0200

    Restored

--- ylrg/src/App.js
+++ ylrg/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+<<<<<<< Updated upstream
 import FormGenerator from './components/FormGenerator'
 
 function App() {
@@ -11,6 +12,21 @@ function App() {
       <div className="item-2">
 
       </div> 
+=======
+
+function App() {
+  
+  function myFunktion() {
+    document.getElementById("test").submit();
+  }
+
+  return (
+    <div>
+      <h1> Hallo Welt !</h1>
+      <input type="text" name="test" id="test" action="/action_page.php"></input>
+      <button type="button" value="Submit" onclick="myFunktion()">Submit</button>
+
+>>>>>>> Stashed changes
     </div>
   );
 }
