c8c8a8e532d7153f019afa46db0b58d55c6352ab
Yul-Leonardo <yulrusch@gmail.com>
2024-05-07 12:57:53 +0200

    Updated!

--- ylrg/src/App.js
+++ ylrg/src/App.js
@@ -3,19 +3,22 @@ import './App.css';
 function App() {
   
   return (
-    <div className='bars'>
-      <h0 className='item-0'></h0>
-      <h1 className='item-1'></h1>
-      <h2 className='item-2'></h2>
-      <h3 className='item-3'></h3>
-      <h4 className='item-4'></h4>
-      <h5 className='item-5'></h5>
-      <h6 className='item-6'></h6>
-      <h7 className='item-7'></h7>
-      <h8 className='item-8'></h8>
-      <h9 className='item-9'></h9>
-      <h10 className='item-10'></h10>
-
+    <div className='site'>
+      <div className='bars'>
+        <div className='bar-top'></div>
+        <div className='bar-left'></div>
+      </div>
+      <div className='container'>
+        <div className='item'>1</div>
+        <div className='item'>2</div>
+        <div className='item'>3</div>
+        <div className='item'>4</div>
+        <div className='item'>5</div>
+        <div className='item'>6</div>
+        <div className='item'>7</div>
+        <div className='item'>8</div>
+        <div className='item'>9</div>
+      </div>
     </div>
   );
 }
