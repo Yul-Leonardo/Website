e288b525bcacea689928c1a0ad020becd843fcbe
Yul-Leonardo <yulrusch@gmail.com>
2025-01-03 21:07:10 +0100

    Updated!

--- ylrg/src/App.js
+++ ylrg/src/App.js
@@ -3,23 +3,7 @@ import './App.css';
 function App() {
   
   return (
-    <div className='site'>
-      <div className='bars'>
-        <div className='bar-top'></div>
-        <div className='bar-left'></div>
-      </div>
-      <div className='container'>
-        <div className='item'>1</div>
-        <div className='item'>2</div>
-        <div className='item'>3</div>
-        <div className='item'>4</div>
-        <div className='item'>5</div>
-        <div className='item'>6</div>
-        <div className='item'>7</div>
-        <div className='item'>8</div>
-        <div className='item'>9</div>
-      </div>
-    </div>
+    <p>BOMBOCLAT</p>
   );
 }
 
